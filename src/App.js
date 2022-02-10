72446155e38585cba817f494f7ed8e5bf1130755
Ornela Anahi Lugo Aquino <nelladelugo2@gmail.com>
2022-02-10 16:12:19 -0300

    Desafío: Estilos y Home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css'
-import NavBar from './components/NavBar'
-
+import NavBar from './components/NavBar/NavBar'
+import Logo from './components/Logos/logo.png'
+import ItemListContainer from './components/itemListContainer/itemListContainer'
 function App () {
   return (
     <>
       <header className='header'>
+        <img src={Logo} className='logo' />
         <h1 className='tituloPrincipal'>Todo para vos</h1>
         <NavBar />
       </header>
+      <ItemListContainer />
     </>
   )
 }
---
+++ src/components/itemListContainer/itemListContainer.jsx
@@ -0,0 +1,12 @@
+export default function ItemListContainer () {
+  return (
+    //   Texto provisional
+    <body>
+      <h2>Nuestros contactos</h2>
+      <p>Facebook</p>
+      <p>Whatsapp</p>
+      <p>Gmail</p>
+      <p>Instagram</p>
+    </body>
+  )
+}
